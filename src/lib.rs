8a24e0744430e6cba8f7aa1f2bf96563f457994c
Alexander Fleming <alexander.fleming@rwth-aachen.de>
2024-03-05 14:15:58 +0100

    Internal mutation via RefCell!

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
 use num::traits::real::Real;
 use num::{Num, NumCast, One, ToPrimitive, Zero};
+use std::cell::RefCell;
 use std::ops::{Add, Div, Mul, Neg, Rem, Sub};
 
 // use std::ops::{AddAssign, DivAssign, MulAssign, SubAssign};
@@ -21,52 +22,83 @@ pub struct Tangent<T> {
     dv: T,
 }
 
-pub struct Adjoint<'tape, T, TAPE> {
+pub struct Adjoint<'tape, T, TAPE>
+where
+    T: Copy + Num,
+    TAPE: AdjointTape<T> + ?Sized,
+{
     v: T,
     id: usize,
-    tape: &'tape mut TAPE,
+    tape: &'tape TAPE,
+}
+
+impl<'tape, T, TAPE> Adjoint<'tape, T, TAPE>
+where
+    T: Copy + Num,
+    TAPE: AdjointTape<T>,
+{
 }
 
 pub trait AdjointTape<T>
 where
     T: Copy + Num,
 {
-    fn num_inputs(&self) -> usize;
-    fn num_outputs(&self) -> usize;
+    /// Get the number of independent variables on this tape.
+    fn num_independents(&self) -> usize;
+    /// Get the number of dependent variables on this tape.
+    fn num_dependents(&self) -> usize;
+    /// Get a new independent adjoint variable on this tape
+    /// with its value intialized to `v`
+    fn new_independent_variable<'tape>(&'tape mut self, v: T) -> Adjoint<'tape, T, Self>;
 
-    fn adjoint_vector_size(self) -> usize;
-    fn empty_seed(self) -> Vec<T>;
+    /// Get the size of the adjoint vector necessary to fully interpret the tape.
+    /// That is, how many elements of `T` must be allocated to to interpret the tape?
+    fn adjoint_vector_size(&self) -> usize;
+    /// Get an empty adjoint vector (intialized to `T::zero`)
+    fn empty_seed(&self) -> Vec<T>;
 
+    /// Get the size of the vector of dependencies.
     fn num_dependencies(&self) -> usize;
-    fn num_derivatives(&self) -> usize;
+    /// Get the size of the vector of derivative information.
+    fn num_pullbacks(&self) -> usize;
+    /// Get the number of adjoint variables allocated during recording.
     fn num_adjoints(&self) -> usize;
 
-    fn record_derivative(&mut self, vbar: T);
-    fn record_result(&mut self, v: T);
+    /// Record a derivative `vbar` with respect to `v` to the tape.
+    fn record_derivative<'a>(&self, v: &Adjoint<'a, T, Self>, vbar: T);
+    /// Record `result`, which has been produced by an operation with `nargs` arguments, onto the tape.
+    /// Updates the `id` of the result.
+    fn record_result<'a>(&self, result: &mut Adjoint<'a, T, Self>, nargs: usize);
+    /// Reset the tape.
     fn scrub(&mut self);
 
-    fn adjoint_vector_idx(&self, tape_position: usize) -> usize;
+    /// Get the position where `adjoint_id` is stored in the vector of adjoints
+    fn adjoint_vector_idx(&self, adjoint_id: usize) -> usize;
+    /// Get the pullback value at `tape_position`
     fn taped_pullback_value_at(&self, tape_position: usize) -> T;
+    /// Get the dependency tape information at `tape_position`
     fn taped_dependency_at(&self, tape_position: usize) -> usize;
 
     fn interpret(&self, seed: &mut Vec<T>) {
         let mut dependency_tape_pos: usize = self.num_dependencies() - 1;
-        let mut pullback_tape_pos: usize = self.num_derivatives() - 1;
+        let mut pullback_tape_pos: usize = self.num_pullbacks() - 1;
         // the first entries on the tape are the labels for the independent vars
         // so we don't need to keep reading backwards to their dependencies
         // since there aren't any
-        while dependency_tape_pos > self.num_inputs() - 1 {
+        while dependency_tape_pos > self.num_independents() - 1 {
             // portions of the dependency tape look like this:
             // ... ID1 ID2 ID3 NARGS ADJID ...
             // so we read backwards, decrementing by 1 after acquiring piece of information
-            let result_adj_idx: usize = self.adjoint_vector_idx(dependency_tape_pos);
+            let result_id: usize = self.taped_dependency_at(dependency_tape_pos);
+            let result_adj_idx: usize = self.adjoint_vector_idx(result_id);
             dependency_tape_pos -= 1;
             let num_pullbacks: usize = self.taped_dependency_at(dependency_tape_pos);
             dependency_tape_pos -= 1;
             let result_pullback: T = seed[result_adj_idx];
             seed[result_adj_idx] = T::zero();
             for _ in 0..num_pullbacks {
-                let pullback_idx: usize = self.adjoint_vector_idx(dependency_tape_pos);
+                let pb_id: usize = self.taped_dependency_at(dependency_tape_pos);
+                let pullback_idx: usize = self.adjoint_vector_idx(pb_id);
                 dependency_tape_pos -= 1;
                 let arg_pullback: T = self.taped_pullback_value_at(pullback_tape_pos);
                 pullback_tape_pos -= 1;
@@ -77,68 +109,113 @@ where
 }
 
 pub struct MinimalTape<T> {
-    dependencies: Vec<usize>,
-    pullbacks: Vec<T>,
+    n_inputs: usize,
+    n_outputs: usize,
+    n_adjoints: RefCell<usize>,
 
-    n_in: usize,
-    n_out: usize,
-    n_adjoints: usize,
+    adj_deps: RefCell<Vec<usize>>,
+    pullbacks: RefCell<Vec<T>>,
+}
+
+impl<T> MinimalTape<T>
+where
+    T: Copy + Num,
+{
+    fn new() -> MinimalTape<T> {
+        MinimalTape {
+            n_inputs: 0,
+            n_outputs: 0,
+            n_adjoints: RefCell::new(0),
+            adj_deps: RefCell::new(vec![usize::zero(); 0]),
+            pullbacks: RefCell::new(vec![T::zero(); 0]),
+        }
+    }
 }
 
 impl<T> AdjointTape<T> for MinimalTape<T>
 where
     T: Copy + Num,
 {
-    fn num_inputs(&self) -> usize {
-        self.n_in
+    #[inline]
+    fn num_independents(&self) -> usize {
+        self.n_inputs
     }
 
-    fn num_outputs(&self) -> usize {
-        self.n_out
+    #[inline]
+    fn num_dependents(&self) -> usize {
+        self.n_outputs
     }
 
-    fn adjoint_vector_size(self) -> usize {
+    fn new_independent_variable<'tape>(&'tape mut self, val: T) -> Adjoint<'tape, T, Self> {
+        let new_id: usize = { *(self.n_adjoints.borrow()) };
+        self.n_adjoints.replace_with(|&mut n| n + 1);
+        Adjoint {
+            v: val,
+            id: new_id,
+            tape: self,
+        }
+    }
+
+    #[inline]
+    fn adjoint_vector_size(&self) -> usize {
         self.num_adjoints()
     }
 
-    fn empty_seed(self) -> Vec<T> {
+    #[inline]
+    fn empty_seed(&self) -> Vec<T> {
         vec![T::zero(); self.adjoint_vector_size()]
     }
 
+    #[inline]
     fn num_dependencies(&self) -> usize {
-        self.dependencies.len()
+        self.adj_deps.borrow().len()
     }
 
-    fn num_derivatives(&self) -> usize {
-        self.pullbacks.len()
+    #[inline]
+    fn num_pullbacks(&self) -> usize {
+        self.pullbacks.borrow().len()
     }
 
+    #[inline]
     fn num_adjoints(&self) -> usize {
-        self.n_adjoints
+        *(self.n_adjoints.borrow())
     }
 
-    fn record_derivative(&mut self, vbar: T) {
-        todo!()
+    fn record_derivative<'a>(&self, v: &Adjoint<'a, T, MinimalTape<T>>, vbar: T) {
+        self.adj_deps.borrow_mut().push(v.id);
+        self.pullbacks.borrow_mut().push(vbar);
     }
 
-    fn record_result(&mut self, v: T) {
-        todo!()
+    fn record_result<'a>(&self, v: &mut Adjoint<'a, T, MinimalTape<T>>, nargs: usize) {
+        v.id = *(self.n_adjoints.borrow());
+        self.n_adjoints.replace_with(|&mut n| n + 1);
+        let mut adj_deps = self.adj_deps.borrow_mut();
+        adj_deps.push(nargs);
+        adj_deps.push(v.id);
+        
     }
 
     fn scrub(&mut self) {
-        todo!()
+        self.n_adjoints.replace(0);
+        self.n_inputs = 0;
+        self.n_outputs = 0;
+        self.adj_deps.borrow_mut().clear();
+        self.pullbacks.borrow_mut().clear();
     }
 
-    fn adjoint_vector_idx(&self, tape_position: usize) -> usize {
-        todo!()
+    #[inline]
+    fn adjoint_vector_idx(&self, adjoint_id: usize) -> usize {
+        adjoint_id
     }
 
+    #[inline]
     fn taped_pullback_value_at(&self, tape_position: usize) -> T {
-        todo!()
+        self.pullbacks.borrow()[tape_position]
     }
 
+    #[inline]
     fn taped_dependency_at(&self, tape_position: usize) -> usize {
-        todo!()
+        self.adj_deps.borrow()[tape_position]
     }
 }
 
@@ -175,7 +252,11 @@ impl<T: PassiveValue> PassiveValue for Tangent<T> {
     }
 }
 
-impl<'a, T: PassiveValue, TAPE> PassiveValue for Adjoint<'a, T, TAPE> {
+impl<'a, T, TAPE> PassiveValue for Adjoint<'a, T, TAPE>
+where
+    T: PassiveValue + Copy + Num,
+    TAPE: AdjointTape<T>,
+{
     type PassiveValueType = T::PassiveValueType;
 
     fn passive_value(self) -> Self::PassiveValueType {
